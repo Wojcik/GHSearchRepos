cd2357cf4ae760aec1a1d0da007ef6da8e58434b
Anton Kovalov <kovalev.main@gmail.com>
2019-02-01 18:29:23 +0200

    Initial Commit

---
+++ GenesisHub/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GenesisHub
+//
+//  Created by Anton Kovalov on 2/1/19.
+//  Copyright © 2019 Anton Kovalov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
